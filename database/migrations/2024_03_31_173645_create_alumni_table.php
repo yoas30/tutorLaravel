69a029f09d0c6a516991a080db9c6373831c2a4b
yoas30 <yoas.rinatan30@gmail.com>
2024-04-01 00:41:09 +0700

    new file:   app/Http/Controllers/AlumniController.php 	new file:   database/migrations/2024_03_31_173645_create_alumni_table.php 	new file:   resources/views/tampilan/alumni.blade.php 	modified:   routes/web.php

---
+++ database/migrations/2024_03_31_173645_create_alumni_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alumni', function (Blueprint $table) {
+            $table->bigIncrements('id_alumni');
+            $table->string('nama');
+            $table->string('jurusan');
+            $table->string('kelulusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alumni');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlumniController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/alumni','AlumniController@index');
+Route::get('/alumni', [AlumniController::class, 'index']);
+
+
