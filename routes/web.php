00097b0af02c9e6d3007c7c52961b66e98116596
yoas30 <yoas.rinatan30@gmail.com>
2024-04-01 11:07:54 +0700

    new file:   app/Http/Controllers/Auth/ConfirmPasswordController.php 	new file:   app/Http/Controllers/Auth/ForgotPasswordController.php 	new file:   app/Http/Controllers/Auth/LoginController.php 	new file:   app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/Auth/ResetPasswordController.php 	new file:   app/Http/Controllers/Auth/VerificationController.php 	modified:   app/Http/Controllers/Controller.php 	new file:   app/Http/Controllers/HomeController.php 	modified:   composer.json 	modified:   composer.lock 	modified:   package.json 	modified:   resources/js/app.js 	modified:   resources/js/bootstrap.js 	new file:   resources/js/components/ExampleComponent.vue 	new file:   resources/sass/_variables.scss 	new file:   resources/sass/app.scss 	new file:   resources/views/auth/login.blade.php 	new file:   resources/views/auth/passwords/confirm.blade.php 	new file:   resources/views/auth/passwords/email.blade.php 	new file:   resources/views/auth/passwords/reset.blade.php 	new file:   resources/views/auth/register.blade.php 	new file:   resources/views/auth/verify.blade.php 	new file:   resources/views/home.blade.php 	new file:   resources/views/layouts/app.blade.php 	modified:   routes/web.php 	modified:   vite.config.js

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AlumniController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/alumni','AlumniController@index');
 Route::get('/alumni', [AlumniController::class, 'index']);
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
